0f757cd95eab636ba1f3061f916e08e72a839f30
sayedAminul <89654789+sayedAminul01@users.noreply.github.com>
2025-04-19 22:30:01 +0530

    Update script.js

--- courses/shorts/script.js
+++ courses/shorts/script.js
@@ -1,6 +1,7 @@
 
         // YouTube videos data - Just IDs or links
         let allVideos = [
+                "https://youtube.com/shorts/M5jJ_8KAilc",
                 "https://www.youtube.com/shorts/Y4MwlbvRADE",
                 "https://www.youtube.com/shorts/-wwN-hpEkW0",
                 "https://www.youtube.com/shorts/J_TcIXPKnKs",
