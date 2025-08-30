e47db654fa5a85224fb0eb90388cca2a93fe0715
sayedAminul <89654789+sayedAminul01@users.noreply.github.com>
2025-08-30 22:07:46 +0530

    Update categoryTools.js

--- categorySection/categoryTools.js
+++ categorySection/categoryTools.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             id: 'todo list',
             title: 'todo list',
-            icon: 'fa-solid fa-hand-holding-dollar',
+            icon: 'fa-solid fa-list-check',
             postCount: "A smart to-do list app designed to simplify your life. Plan tasks, set priorities, and achieve more every day.",
             description: 'Loan_Tools',
             url: 'https://todo.servics.xyz/'
@@ -128,4 +128,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
